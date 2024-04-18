ba53cf2e4053d557ef20606b0574d15acb084194
lyth hammad <bigleo419@gmail.com>
2024-04-17 20:05:36 -0500

    started with the fronend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,23 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setContacts] = useState([])
+
+  useEffect(() => {
+    fetchContacts()
+  }, [])
+
+  const fetchContacts = async () => {
+    const response = await fetch('http://127.0.0.1:5000/contacts')
+    const data = await response.json()
+    setContacts(data.contacts)
+    console.log(data.contacts)
+  }
 
   return (
     <>
+    
     </>
   )
 }
---
+++ frontend/src/ContactsList.jsx
@@ -0,0 +1,31 @@
+import React from "react"
+
+const ContactList = ({contacts}) => {
+    return <dev>
+        <h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Email</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {contacts.map(contact => (
+                        <tr key={contact.id}>
+                            <td>{contact.firstName}</td>
+                            <td>{contact.lastName}</td>
+                            <td>{contact.email}</td>
+                            <td>
+                                <button>Edit</button>
+                                <button>Delete</button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </h2>
+    </dev>
+}
